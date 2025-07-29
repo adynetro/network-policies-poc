83243c6400327730e12b1887f053bddb59db3ad1
Alexandru Chișcari <alexandru.chiscari@btrl.ro>
2025-07-29 17:17:54 +0300

    fuck gpt

--- deploy-resources.sh
+++
@@ -1,5 +0,0 @@
-kubectl apply -f manifest/namespace.yaml
-kubectl apply -f manifest/frontend-deployment.yaml
-kubectl apply -f manifest/backend-deployment.yaml
-kubectl apply -f manifest/frontend-service.yaml
-kubectl apply -f manifest/backend-service.yaml
